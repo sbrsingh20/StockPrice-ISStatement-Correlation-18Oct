d51620c88f92dd9a7175af778e1a7f845a357e18
sbrsingh20 <sbrsingh20@gmail.com>
2024-10-18 21:55:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ expected_event_rate = st.sidebar.number_input('Enter Expected Upcoming Rate (%):
 # Function to fetch income statement data for the selected stock from the financials folder
 def fetch_income_statement(stock_symbol):
     folder_path = 'financials'
+    stock_data = None
     for filename in os.listdir(folder_path):
         if filename.endswith('.xlsx'):
             file_path = os.path.join(folder_path, filename)
